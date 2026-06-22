0be307c2cf50a433c4beac520cb4b72c3036be80
Rezal <rezalsharma@gmail.com>
2026-06-22 00:37:36 -0700

    Add files via upload

---
+++ lec 6.py
@@ -0,0 +1,16 @@
+# fp=open("myfile.txt",'w')
+# msg=input("Enter a message:")
+# lines=msg.split(".")
+# for line in lines:
+#     fp.write(line+".\n")
+# fp.close()
+
+
+# with open("myfile.txt",'r')as fb:
+#     data=fb.read(6)
+#     print(data)
+#     print(fb.tell())
+#     fb.seek(3)
+#     print(fb.tell())
+
+
